667625a02f8f654692693f09cd8a2820a1c36ee7
mantas.p <man.petrikas@gmail.com>
2017-12-19 12:53:54 +0200

    Compile fix

--- lab2/generate.ps1
+++
@@ -1,9 +0,0 @@
-xelatex PST2.tex -interaction nonstopmode;
-xelatex PST2.tex -interaction nonstopmode;
-Remove-Item PST2.toc;
-Remove-Item PST2.run.xml;
-Remove-Item PST2.out;
-Remove-Item PST2.log;
-Remove-Item PST2.bcf;
-Remove-Item PST2.aux;
-start PST2.pdf
